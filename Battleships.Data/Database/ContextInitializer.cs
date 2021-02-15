523af7a3adb809f85308e0237e69954d9e092aec
Vainiusst <vainius1994@gmail.com>
2021-02-15 21:43:36 +0200

    Updated the DB part

---
+++ Battleships.Data/Database/ContextInitializer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Battleships.Data.Database
+{
+    public class ContextInitializer : CreateDatabaseIfNotExists<MyDbContext>
+    {
+        protected override void Seed(MyDbContext context)
+        {
+            //base.Seed(context);
+        }
+    }
+}
---
+++ Battleships.Data/Database/MyDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Battleships.Data.Database
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext() : base("BattleshipsDB")
+        {
+            System.Data.Entity.Database.SetInitializer(new ContextInitializer());
+        }
+    }
+}
